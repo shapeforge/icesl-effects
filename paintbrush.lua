f898beb5a8926e89ad13ccc123ea91b40bf4380a
Salim Perchy <salim.perchy@gmail.com>
2018-12-04 11:15:21 +0100

    added proper ui_field description, spanish translation

--- paintbrush.lua
+++ paintbrush.lua
@@ -1,25 +1,25 @@
 name_en = "Paint brushes"
-name_es = ""
+name_es = "Pintar brochas"
 name_fr = "Peindre les brush"
 name_ch = "Paint brushes"
 description_en = "Paint brush selection"
-description_es = ""
+description_es = "Pintar con dos brochas"
 description_fr = "Peindre la sélection de brush"
 description_ch = "Paint brush selection"
 
 function effect(mdl)
   bx = bbox(mdl)
-  f = ui_field('first brush',bx:min_corner(),bx:max_corner(),true)
+  f = ui_field('Brush 0  (< 0.5)\nBrush 2  (> 0.5)',bx:min_corner(),bx:max_corner(),true)
   solid = to_voxel_solid(f,bx:min_corner(),bx:max_corner())
   emit(
       intersection(
       mdl,
-      solid),1
+      solid),0
   )
   emit(
       difference(
       mdl,
-      solid),0
+      solid),2
   )
   return Void
 end
