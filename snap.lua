9923e8ac166162d0cc710f4e9d4584f682ebd183
Salim Perchy <salim.perchy@gmail.com>
2022-03-01 16:59:54 +0100

    corrected order in snap effect

--- snap.lua
+++ snap.lua
@@ -1,18 +1,18 @@
 name_en = "Snap to bed"
 name_es = "Fijar a la base"
-name_fr = "Fixe au lit"
+name_fr = "Fixe au plateau"
 description_en = "Snaps the object to the printing bed based on a surface point"
 description_es = "Fija el objecto a la base de impresión con respecto a un punto de su superficie"
-description_fr = "Fixe l'object au lit d'impression à l'égard d'un point de sa surface"
+description_fr = "Fixe l'objet au lit d'impression à l'égard d'un point de sa surface"
 
 enable_variable_cache = true
 
 function effect(mdl)
-	local snap_model = ui_bool('Snap object to bed w.r.t.\npoint (close surface picker first)', false)
-
-	if not snap_model then
+  if not snap_model then
 	  pmatrix = ui_pick('Choose surface point')
 	end
+
+	local snap_model = ui_bool('Snap object to bed w.r.t.\npoint (close surface picker first)', false)
 
 	local mag1 = union(Void, magnet('m1'))
 	local mag2 = union(Void, pmatrix * magnet('m2'))
